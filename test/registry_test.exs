6a9818ccb843e3dcf1caf2ffe4a94a1f58d429ed
HoussemFerjaoui <houssem.ferjaoui@outlook.com>
2024-10-16 00:57:19 +0100

    adding test for GenServer registry

---
+++ test/registry_test.exs
@@ -0,0 +1,20 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, server} = KV.Registry.start_link([])
+    %{server: server}
+  end
+
+  test "returns error for not found bucket lookup", %{server: server} do
+    assert KV.Registry.lookup(server, "404_bucket") == :error
+  end
+
+  test "create & look up a bucket", %{server: server} do
+    assert :ok = KV.Registry.create(server, "new_bucket")
+    assert {:ok, bucket} = KV.Registry.lookup(server, "new_bucket")
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+end
