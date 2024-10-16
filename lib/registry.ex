48a61c23fac0a41bee097b55b22ea5b8c0977d7a
HoussemFerjaoui <houssem.ferjaoui@outlook.com>
2024-10-16 23:07:48 +0100

    Implement monitor and add refs to genserver state

--- lib/registry.ex
+++ lib/registry.ex
@@ -13,8 +13,16 @@ defmodule KV.Registry do
 
   Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
   """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+  def lookup_bucket(server, name) do
+    GenServer.call(server, {:lookup_name, name})
+  end
+
+  def lookup_monitor(server, name) do
+    GenServer.call(server, {:lookup_ref, name})
+  end
+
+  def print_state(server) do
+    GenServer.call(server, :print_state)
   end
 
   @doc """
@@ -27,21 +35,55 @@ defmodule KV.Registry do
   # GenServer Callbacks
   @impl true
   def init(:ok) do
-    {:ok, %{}}
+    names = %{}
+    refs = %{}
+    {:ok, {names, refs}}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, names_list) do
-    {:reply, Map.fetch(names_list, name), names_list}
+  def handle_call({:lookup_name, name}, _from, state) do
+    {names, _} = state
+    {:reply, Map.fetch(names, name), state}
   end
 
   @impl true
-  def handle_cast({:create, name}, names_list) do
+  def handle_call({:lookup_ref, name}, _from, state) do
+    {_, refs} = state
+    {:reply, Map.fetch(refs, name), state}
+  end
+
+  @impl true
+  def handle_call(:print_state, _from, state) do
+    IO.inspect(state)
+    {:reply, :ok, state}
+  end
+
+  @impl true
+  # Attach monitor on bucket creation
+  def handle_cast({:create, name}, {names_list, refs_list}) do
     if Map.has_key?(names_list, name) do
-      {:noreply, names_list}
+      {:noreply, {names_list, refs_list}}
     else
-      {:ok, new_bucket} = KV.Bucket.start_link()
-      {:noreply, Map.put(names_list, name, new_bucket)}
+      {:ok, bucket} = KV.Bucket.start_link()
+      ref = Process.monitor(bucket)
+      refs_list = Map.put(refs_list, ref, name)
+      names_list = Map.put(names_list, name, bucket)
+      {:noreply, {names_list, refs_list}}
     end
+  end
+
+  @impl true
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    # using pop so we it returns the poped name, so we can use name to delte it from names
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  @impl true
+  def handle_info(msg, state) do
+    require Logger
+    Logger.debug("Unexpected message in KV.Registry: #{inspect(msg)}")
+    {:noreply, state}
   end
 end
--- test/registry_test.exs
+++ test/registry_test.exs
@@ -12,14 +12,25 @@ defmodule KV.RegistryTest do
   end
 
   test "returns error for not found bucket lookup", %{server: server} do
-    assert KV.Registry.lookup(server, "404_bucket") == :error
+    assert KV.Registry.lookup_bucket(server, "404_bucket") == :error
   end
 
   test "create & look up a bucket", %{server: server} do
     assert :ok = KV.Registry.create(server, "new_bucket")
-    assert {:ok, bucket} = KV.Registry.lookup(server, "new_bucket")
+    assert {:ok, bucket} = KV.Registry.lookup_bucket(server, "new_bucket")
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "when agent is dead, server should remove the agent as well", %{server: server} do
+    KV.Registry.create(server, "my_bucket")
+    {:ok, bucket} = KV.Registry.lookup_bucket(server, "my_bucket")
+    assert :ok = Agent.stop(bucket)
+    assert KV.Registry.lookup_bucket(server, "my_bucket") == :error
+  end
+
+  test "print the server state", %{server: server} do
+    assert :ok = KV.Registry.print_state(server)
   end
 end
